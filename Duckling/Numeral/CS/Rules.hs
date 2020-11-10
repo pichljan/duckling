d48bffd506b0af617c32b6cc9359bec3215851a1
Jan Pichl <honza.pichl@gmail.com>
2020-11-10 18:23:06 +0100

    Regex fixes: unique rule names, matching parentheses, unicode chars

--- Duckling/Numeral/CS/Rules.hs
+++ Duckling/Numeral/CS/Rules.hs
@@ -27,82 +27,82 @@ import Duckling.Regex.Types
 import Duckling.Types
 import qualified Duckling.Numeral.Types as TNumeral
 
-ruleNumeralMap :: HashMap Text Integer
-ruleNumeralMap = HashMap.fromList
+zeroNineMap :: HashMap Text Integer
+zeroNineMap = HashMap.fromList
   [ ( "nula", 0 )
   , ( "jeden", 1 )
   , ( "jedna", 1 )
   , ( "jedno", 1 )
   , ( "dva", 2 )
-  , ( "dv\x0115", 2 )
-  , ( "t\x0159i", 3 )
-  , ( "čty\x0159i", 4 )
-  , ( "p\x0115t", 5)
+  , ( "dvě", 2 )
+  , ( "tři", 3 )
+  , ( "čtyři", 4 )
+  , ( "pět", 5)
   , ( "šest", 6)
   , ( "sedm", 7)
   , ( "osm", 8)
-  , ( "dev\x0115t", 9)
+  , ( "devět", 9)
   ]
 
-ruleNumeral :: Rule
-ruleNumeral = Rule
-  { name = "number (0..10)"
+rulesToNine :: Rule
+rulesToNine = Rule
+  { name = "number (0..9)"
   , pattern =
-    [ regex "(nula|jed(en|n[ao])|dv(a|\x0115)|t(\x0159)i|(č)ty(\x0159)i|p(\x0115)t|(š)est|sedm|osm|dev(\x0115)t)"
+    [ regex "(nula|jed(en|n[ao])|dv(a|ě)|tři|čtyři|pět|šest|sedm|osm|devět)"
     ]
   , prod = \tokens -> case tokens of
       (Token RegexMatch (GroupMatch (match:_)):_) ->
-        HashMap.lookup (Text.toLower match) ruleNumeralMap >>= integer
+        HashMap.lookup (Text.toLower match) zeroNineMap >>= integer
       _ -> Nothing
   }
 
-dozenMap :: HashMap Text Integer
-dozenMap = HashMap.fromList
-  [ ( "jeden(\x0161)ct"       , 11)
-  , ( "dvan(\x0161)ct"        , 12)
-  , ( "t(x\0281)in(\x0161)ct" , 13)
-  , ( "(x\0201)trn(\x0161)ct" , 14)
-  , ( "patn(\x0161)ct"        , 15)
-  , ( "(\x0289)estn(\x0161)ct", 16)
-  , ( "sedmn(\x0161)ct"       , 17)
-  , ( "osmn(\x00161)ct"       , 18)
-  , ( "devaten(\x0161)ct"     , 19)
+elevenNineteenMap :: HashMap Text Integer
+elevenNineteenMap = HashMap.fromList
+  [ ( "jedenáct"       , 11)
+  , ( "dvanáct"        , 12)
+  , ( "třináct"        , 13)
+  , ( "čtrnáct"        , 14)
+  , ( "patnáct"        , 15)
+  , ( "šestnáct"       , 16)
+  , ( "sedmnáct"       , 17)
+  , ( "osmnáct"        , 18)
+  , ( "devatenáct"     , 19)
   ]
 
-ruleNumeral :: Rule
-ruleNumeral = Rule
-  { name = "number (0..10)"
+rulesElevenToNineteen :: Rule
+rulesElevenToNineteen = Rule
+  { name = "number (11..19)"
   , pattern =
-    [ regex "(jeden(\x0161)ct|dvan(\x0161)ct|t(x\0281)in(\x0161)ct|(x\0201)trn(\x0161)ct|patn(\x0161)ct|(\x0289)estn(\x0161)ct|sedmn(\x0161)ct|osmn(\x00161)ct|devaten(\x0161)ct)"
+    [ regex "(jedenáct|dvanáct|třináct|čtrnáct|patnáct|šestnáct|sedmnáct|osmnáct|devatenáct)"
     ]
   , prod = \tokens -> case tokens of
       (Token RegexMatch (GroupMatch (match:_)):_) ->
-        HashMap.lookup (Text.toLower match) ruleNumeralMap >>= integer
+        HashMap.lookup (Text.toLower match) elevenNineteenMap >>= integer
       _ -> Nothing
   }
 
-  dozenMap :: HashMap Text Integer
-dozenMap = HashMap.fromList
+tensMap :: HashMap Text Integer
+tensMap = HashMap.fromList
   [ ( "deset"                 , 10)
   , ( "dvacet"                , 20)
-  , ( "t(x\0281)icet"         , 30)
-  , ( "(x\0201)ty(x\0281)icet", 40)
-  , ( "pades(\x0161)t"        , 50)
-  , ( "(\x0289)edes(\x0161)t" , 60)
-  , ( "sedmdes(\x0161)t"      , 70)
-  , ( "osmndes(\x00161)t"     , 80)
-  , ( "devades(\x0161)t"      , 90)
+  , ( "třicet"                , 30)
+  , ( "čtyřicet"              , 40)
+  , ( "padesát"               , 50)
+  , ( "šedesát"               , 60)
+  , ( "sedmdesát"             , 70)
+  , ( "osmndesát"             , 80)
+  , ( "devadesát"             , 90)
   ]
 
-ruleNumeral :: Rule
-ruleNumeral = Rule
-  { name = "number (0..10)"
+ruleTens :: Rule
+ruleTens = Rule
+  { name = "number (10..90)"
   , pattern =
-    [ regex "(deset|dvacet|t(x\0281)icet|(x\0201)ty(x\0281)icet|pades(\x0161)t|(\x0289)edes(\x0161)t|sedmdes(\x0161)t|osmndes(\x00161)t|devades(\x0161)t"
+    [ regex "(deset|dvacet|třicet|čtyřicet|padesát|šedesát|sedmdesát|osmndesát|devadesát)"
     ]
   , prod = \tokens -> case tokens of
       (Token RegexMatch (GroupMatch (match:_)):_) ->
-        HashMap.lookup (Text.toLower match) ruleNumeralMap >>= integer
+        HashMap.lookup (Text.toLower match) tensMap >>= integer
       _ -> Nothing
   }
 
@@ -110,5 +110,7 @@ ruleNumeral = Rule
 
 rules :: [Rule]
 rules =
-  [ ruleNumeral
+  [ rulesToNine
+  , rulesElevenToNineteen
+  , ruleTens
   ]
